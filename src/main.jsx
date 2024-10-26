beac9b6bd86b68a5441582584cd38cbbb366911b
Dmytro Reviakin <thefaust31@gmail.com>
2024-10-26 14:52:05 +0300

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
